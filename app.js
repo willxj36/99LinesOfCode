f8debe976ebb0ca2626cccdc08f58c50c54d1b4e
willxj36 <willxj36@gmail.com>
2020-12-29 15:26:23 -0700

    added html and JS (JS has errors)

---
+++ app.js
@@ -0,0 +1,7 @@
+let friends = ["Ethan", "Kendra", "Adam", "Amber", "Rudy"];
+
+for (i = 0; i < friends.length; i++) {
+    for (j = 99; j > 0; j--) {
+        console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + j-1 +" lines of code in the file");
+    };
+};
