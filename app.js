36efa2692b66b2d8b9797115dd8f42192ec30ae5
willxj36 <willxj36@gmail.com>
2020-12-29 18:29:22 -0700

    added css, finished js, modified html

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let friends = ["Ethan", "Kendra", "Adam", "Amber", "Rudy"];
 
 for (i = 0; i < friends.length; i++) {
     for (j = 99; j > 0; j--) {
-        console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + j-1 +" lines of code in the file");
+        if((j-1) > 0) {
+            console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, " + (j-1) +" lines of code in the file");
+        } else {console.log(j + " lines of code in the file, " + j + " lines of code; " + friends[i] + " strikes one out, clears it all out, no more lines of code in the file");
+        };
     };
 };
